db8d135a4b44b1624a4438e11ff95bdb6f0d3f00
ameyBhojane <abrock77@gmail.com>
2024-09-16 22:35:39 +0530

    cors erre

--- app.js
+++ app.js
@@ -14,16 +14,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(cors({
-  origin: '*'
-}));
-
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
-});
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
